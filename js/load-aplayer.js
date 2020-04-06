69025eb828d66f202ce44a8149b64bac3682f49e
Author Name <Author Email>
2020-04-06 08:57:35 +0800

    Site updated: 2020-04-06 08:57:33

---
+++ js/load-aplayer.js
@@ -0,0 +1,17 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>document.addEventListener("DOMContentLoaded", function() {
+  let apContainer = document.createElement("div");
+  apContainer.id = "aplayer";
+  document.body.append(apContainer);
+  const ap = new APlayer({
+    container: document.getElementById("aplayer"),
+    fixed: true,
+    audio: [
+      {
+        name: "name",
+        artist: "artist",
+        url: "url.mp3",
+        cover: "cover.jpg"
+      }
+    ]
+  });
+});
