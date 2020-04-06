554d8007252c6b5566c01db475d7b550f4f847a0
Author Name <Author Email>
2020-04-06 08:38:43 +0800

    :pencil: update content

--- source/about/site.md
+++ source/about/site.md
@@ -1,4 +1,4 @@
 ---
-title: About
+title: About Site
 date: 2020-04-05 21:16:48
 ---
---
+++ source/js/load-aplayer.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let apContainer = document.createElement("div");
+  apContainer.id = "aplayer";
+  document.body.append(apContainer);
+  const ap = new APlayer({
+    container: document.getElementById("aplayer"),
+    fixed: true,
+    audio: [
+      {
+        name: "name",
+        artist: "artist",
+        url: "url.mp3",
+        cover: "cover.jpg"
+      }
+    ]
+  });
+});
