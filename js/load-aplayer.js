7423a3bcf77cdc67268fcb3e7e1f112810e5494b
Author Name <Author Email>
2020-05-02 10:51:17 +0800

    Update Hexo Static Content

--- js/load-aplayer.js
+++ js/load-aplayer.js
@@ -1,16 +1,17 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="https://cdn.jsdelivr.net/npm/aplayer@latest/dist/APlayer.min.css"><script src="https://cdn.jsdelivr.net/npm/aplayer@latest/dist/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="https://cdn.jsdelivr.net/npm/meting@1/dist/Meting.min.js"></script>document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
   let apContainer = document.createElement("div");
   apContainer.id = "aplayer";
   document.body.append(apContainer);
   const ap = new APlayer({
     container: document.getElementById("aplayer"),
     fixed: true,
+    autoplay: true,
     audio: [
       {
-        name: "name",
-        artist: "artist",
-        url: "url.mp3",
-        cover: "cover.jpg",
+        name: "鱼",
+        artist: "西瓜JUN",
+        url: "http://m10.music.126.net/20200502104425/a96fdfa6dd916b9475c0d56dd2cab214/ymusic/0f53/075d/0f0c/893e5cc3f8073ec68ac68fd1e0bbc328.mp3",
+        cover: "http://p2.music.126.net/qVk9flKKqmFF30MfXKHcqA==/109951163655933659.jpg?param=300x300",
       },
     ],
   });
