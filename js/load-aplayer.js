a9e0520563d9ef62e2c54790e6d4e90c2fc8b7f8
Author Name <Author Email>
2020-05-02 11:22:36 +0800

    Update Hexo Static Content

--- js/load-aplayer.js
+++ js/load-aplayer.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
       {
         name: "鱼",
         artist: "西瓜JUN",
-        url: "http://m10.music.126.net/20200502104425/a96fdfa6dd916b9475c0d56dd2cab214/ymusic/0f53/075d/0f0c/893e5cc3f8073ec68ac68fd1e0bbc328.mp3",
+        url: "http://music.163.com/song/media/outer/url?id=1324078465.mp3",
         cover: "http://p2.music.126.net/qVk9flKKqmFF30MfXKHcqA==/109951163655933659.jpg?param=300x300",
       },
     ],
